9c7f0c3d61091782ca0414ff667ec6df20a7eff5
Jihyun Kim <ang21icjh@gmail.com>
2022-05-27 03:05:33 +0900

    fix: put random letter in random index

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,36 +4,43 @@ import Navbar from "../Components/Navbar";
 const Home = () => {
   const [length, setLength] = useState(8);
   const [isUpperCase, setIsUpperCase] = useState(false);
-  const [isLowerCase, setIsLowerCase] = useState(true);
+  const [isLowerCase, setIsLowerCase] = useState(false);
   const [symbols, setSymbols] = useState(false);
+  // By the way, we forgot to consider number case
 
   const formHandler = (e) => {
     e.preventDefault();
-    let password = Math.floor(Math.random() * Math.pow(10, length)).toString();
+    if (!(isUpperCase || isLowerCase || symbols)) return;
+
+    // let password = Math.floor(Math.random() * Math.pow(10, length)).toString();
+    const password = new Array(length);
 
     const characters = "abcdefghijklmnopqrstuvwxyz";
-    characters[0] = "A";
-    console.log(characters[0]);
+    // characters[0] = "A"; // string is primitive type, so it's immutable
 
     if (isLowerCase) {
-      //   1/3 of the password should be lower case letters
-
+      // 1/3 of the password should be lower case letters
       const amount = password.length / 3;
 
-      for (let index = 0; index < 4; index++) {
-        let randomIndex = Math.floor(Math.random() * (password.length - 1));
+      for (let index = 0; index < amount; index++) {
+        let randomIndex;
+        do {
+          randomIndex = Math.floor(Math.random() * password.length);
+          // I think it should be calculated in this way.
+          // When password.length is 8, the maximum randomIndex should be 7.
+          // Math.random() is always lower than 1, so Math.random() * 8 is between 0 and 7.xxx
+          // After applying Math.floor(), it will become an integer between 0 and 7.
+          // Do you think it's correct? :O
+        } while (password[randomIndex]); // This is to prevent getting same randomIndex in the previous for loops
 
         let randomLetter =
-          characters[Math.floor(Math.random() * (characters.length - 1))];
+          characters[Math.floor(Math.random() * characters.length)];
 
         password[randomIndex] = randomLetter;
-        console.log(password);
       }
-
-      console.log("lowercase");
     }
 
-    console.log(password);
+    console.log(password.join("")); // Now it returns string that consists of random lowercase letters
   };
 
   const lengthHandler = (e) => {
@@ -56,6 +63,7 @@ const Home = () => {
       <Navbar />
       <form onSubmit={formHandler}>
         <input
+          type="number"
           placeholder="Length of the password"
           onChange={lengthHandler}
           value={length}
